51f59c170b1853ff766d440810e66063c79ae55d
Bach Nguyen <bachtothefuture@Bachs-MacBook-Pro.local>
2020-06-17 12:09:02 -0500

    help page

---
+++ src/components/HelpContainer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './HelpContainer.css';
+
+const HelpContainer: React.FC = () => {
+  return (
+    <div className="container">
+      <strong>Helpful tips/intro go here</strong>
+    </div>
+  );
+};
+
+export default HelpContainer;
--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
+import HelpContainer from '../components/HelpContainer';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
   return (
     <IonPage>
       <IonContent>
-      <IonToolbar>
-        <IonTitle size="large">Large Title</IonTitle>
-      </IonToolbar>
-        <ExploreContainer name="Tab 1 page" />
+        <IonTitle size="large" class="title">Welcome<br></br><b>BunPillo!</b></IonTitle>
+        <HelpContainer/>
       </IonContent>
     </IonPage>
   );
