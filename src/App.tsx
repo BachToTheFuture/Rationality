79a6c2a19ceb0955f1ef8ea37f0a424c3167729c
Bach Nguyen <bachtothefuture@Bachs-MacBook-Pro.local>
2020-06-17 13:16:54 -0500

    stuff

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import {
   IonTabs
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { ellipse, square, triangle, calendarOutline } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
@@ -48,20 +48,16 @@ const App: React.FC = () => (
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
           <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
+            <IonIcon icon={calendarOutline} />
           </IonTabButton>
           <IonTabButton tab="tab2" href="/tab2">
             <IonIcon icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab3" href="/tab3">
             <IonIcon icon={square} />
-            <IonLabel>Tab 3</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab4" href="/tab4">
             <IonIcon icon={ellipse} />
-            <IonLabel>Tab 4</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
--- src/components/HelpContainer.tsx
+++ src/components/HelpContainer.tsx
@@ -1,10 +1,41 @@
 import React from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonItem, IonIcon, IonLabel, IonButton } from '@ionic/react';
+import { calendarOutline, wifi, wine, warning, walk } from 'ionicons/icons';
 import './HelpContainer.css';
 
 const HelpContainer: React.FC = () => {
   return (
     <div className="container">
       <strong>Helpful tips/intro go here</strong>
+      <IonCard>
+          <IonCardHeader>
+            <IonCardTitle>Your profile</IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent>
+            Blah blah some data about yourself
+          </IonCardContent>
+        </IonCard>
+        <IonCard>
+          <IonItem className="ion-activated">
+            <IonIcon icon={calendarOutline} slot="start" />
+            <IonLabel> Calendar </IonLabel>
+          </IonItem>
+
+          <IonItem>
+            <IonIcon icon={wine} slot="start" />
+            <IonLabel>Card Link Item 2</IonLabel>
+          </IonItem>
+
+          <IonItem className="ion-activated">
+            <IonIcon icon={warning} slot="start" />
+            <IonLabel>Card Button Item 1 activated</IonLabel>
+          </IonItem>
+
+          <IonItem>
+            <IonIcon icon={walk} slot="start" />
+            <IonLabel>Card Button Item 2</IonLabel>
+          </IonItem>
+        </IonCard>
     </div>
   );
 };
