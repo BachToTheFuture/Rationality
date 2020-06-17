d5ef5bc2864f9ae106b359d212148d9558ea37d9
Bach Nguyen <bachtothefuture@Bachs-MacBook-Pro.local>
2020-06-17 11:27:10 -0500

    test

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -6,17 +6,10 @@ import './Tab1.css';
 const Tab1: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
+      <IonToolbar>
+        <IonTitle size="large">Large Title</IonTitle>
+      </IonToolbar>
         <ExploreContainer name="Tab 1 page" />
       </IonContent>
     </IonPage>
