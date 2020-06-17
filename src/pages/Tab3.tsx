13dda60cc839e9207ea8de5e48fb6603f99fc3bc
jendyren <jendyren@gmail.com>
2020-06-17 18:29:52 -0400

    Sign out button

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonIcon, } from '@ionic/react';
 import HelpContainer from '../components/HelpContainer';
+import { star } from 'ionicons/icons';
 import './Tab3.css';
 
 
@@ -11,9 +12,15 @@ const Tab3: React.FC = () => {
         <IonTitle size="large" class="welcome">Good morning,</IonTitle>
         <IonTitle size="large" class="usertitle"><b>BunPillo!</b></IonTitle>
         <HelpContainer/>
+        
+        {/*-- Anchor --*/}
+        <IonButton class="center" href="#" color="tertiary" shape="round">Sign Out</IonButton>
       </IonContent>
     </IonPage>
   );
 };
+
+
+
 
 export default Tab3;
