94a7ed2ab4b250ccdf21aae19c72abce09958e0a
Bach Nguyen <bach5000@gmail.com>
2020-06-17 23:43:22 -0500

    search functionality

--- src/components/RecipeList.tsx
+++ src/components/RecipeList.tsx
@@ -5,15 +5,49 @@ import { calendarOutline, heartOutline, alarm, wine, warning, walk } from 'ionic
 import './RecipeList.css';
 import RecipeCard from './RecipeCard';
 
-const RecipeList: React.FC = () => {
-  return (
-    <div>
-        <IonSearchbar value="hi"></IonSearchbar>
-        <RecipeCard name="Boba" time="30 min" diff="Easy" favorite={true}/>
-        <RecipeCard name="Fried Rice" time="30 min" diff="Easy" favorite={false}/>
-        <RecipeCard name="Cake" time="30 min" diff="Easy" favorite={false}/>
-    </div>
-  );
-};
+// <RecipeCard name="Boba" time="30 min" diff="Easy" favorite={true}/>
+
+var query = "";
+var data = [];
+
+class RecipeList extends React.Component {
+  state = {
+    search: 0
+  }
+  handleQuery = (e: React.ChangeEvent<HTMLInputElement>) => {
+    query = e.target.value;
+  }
+  search = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    fetch(`https://Rationality--bach5000.repl.co/search/`+query)
+    .then(response => response.json()).then(content => {
+      console.log(content);
+      data = [
+        {
+          component: "RecipeCard",
+          name: "Boba",
+          time: "30 min",
+          diff: "Easy",
+          favorite: true,
+        }
+      ]
+      this.setState({
+        search: this.state.search ? 1 : 0
+      });
+      // How to turn this into components?
+    })
+  }
+  render () {
+    console.log("Render")
+    return (
+      <div>
+        <form onSubmit={this.search}>
+        <input placeholder="Search..." className="textbox" type="text" onChange={this.handleQuery}></input>
+      </form>
+        {data.map(block => <RecipeCard name={block.name} time={block.time} diff={block.diff} favorite={block.favorite}/>)}
+      </div>
+    )
+  }
+}
 
 export default RecipeList;
